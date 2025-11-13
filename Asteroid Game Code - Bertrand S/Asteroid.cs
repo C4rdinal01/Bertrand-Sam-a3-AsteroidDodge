1faa48e9a15604e77341ace24831507394be04de
C4rdinal01 <cardinal01ttv@gmail.com>
2025-11-13 13:06:44 -0500

    add Asteroid.cs

---
+++ Asteroid Game Code - Bertrand S/Asteroid.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Asteroid_Game_Code___Bertrand_S
+{
+    internal class Asteroid
+    {
+        public void Setup()
+        {
+
+        }
+
+        public void Update()
+        {
+
+        }
+    }
+}
