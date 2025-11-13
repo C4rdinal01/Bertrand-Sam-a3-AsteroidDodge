04436625026b08256099af1ab0a60344474709cf
C4rdinal01 <cardinal01ttv@gmail.com>
2025-11-13 13:08:33 -0500

    add ScoreCounter.cs and code

---
+++ Asteroid Game Code - Bertrand S/ScoreCounter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MohawkGame2D
+{
+    public class ScoreCounter
+    {
+
+        int scoreNumber = 0;
+
+        public void Setup()
+        {
+            Text.Color = Color.White;
+            Text.Size = 3;
+            Text.Draw("Score:" + scoreNumber, 5, 5);
+        }
+
+        public void Update()
+        {
+
+        }
+
+        public void Scoring()
+        {
+            // if an asteroid passes through the bottom of the screen, scoreNumber++
+
+            // alternatively it could increase every second the player lives so;
+            // scoreNumber = Time.SecondsElapsed <- or something like this
+        }
+    }
+}
